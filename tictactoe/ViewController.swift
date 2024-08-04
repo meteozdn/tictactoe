fbb631f7dda55e7b6a30627b5d2d772ef1677b08
Metehan Özden <metehanozden.bil@gmail.com>
2024-08-04 20:48:32 +0300

    Initial Commit

---
+++ tictactoe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tictactoe
+//
+//  Created by Metehan Özden on 4.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
