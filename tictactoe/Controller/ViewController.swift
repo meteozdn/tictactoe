b369826b811353191b4590f63164274e276ac0e6
Metehan Özden <metehanozden.bil@gmail.com>
2024-08-04 21:43:40 +0300

    init

--- tictactoe/Controller/ViewController.swift
+++ tictactoe/Controller/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+
+    var playOrder = 0
+    var letter = "X"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+    
+    @IBAction func buttonCellOnTap(_ sender: UIButton) {
+        sender.isEnabled = false
+        sender.setTitle(letter, for: .normal)
+        sender.titleLabel?.font = UIFont.systemFont(ofSize: 90)
+        sender.setTitleColor(.red, for: .normal)
+        play()
+    }
+    
+    func play() {
+        
+        if playOrder == 0{
+            letter = "O"
+            playOrder = 1
+        }else {
+            letter = "X"
+            playOrder = 0
+        }
+    }
+    
 
 }
 
